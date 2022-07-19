5fe1855b81585e59bc017eba0213e2767d5bea79
gurudattpuranik25 <gurudattpuranik35@gmail.com>
2022-07-19 20:21:33 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { FcBriefcase } from "react-icons/fc";
+import MainContainer from "./components/MainContainer/MainContainer";
 import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <MainContainer />
       <FcBriefcase />
     </div>
   );
